489f6e3cfc605c5b0abcd0d970cf13be0e970dfa
LuigiGrimaldi <grimaldi1110@gmail.com>
2023-03-07 15:06:12 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+alert('funziona!');
+
+// Chiedi all'utente il suo nome, (uso prompt per chiedere il nome all'utente)
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
