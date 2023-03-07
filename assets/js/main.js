43e567ce4a68029d61ea4321e2fa76060bfec1c5
LuigiGrimaldi <grimaldi1110@gmail.com>
2023-03-07 15:51:53 +0100

    request commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
-alert('funziona!');
+
 
 // Chiedi all'utente il suo nome, (uso prompt per chiedere il nome all'utente)
-// poi chiedi il suo cognome,
-// poi chiedi il suo colore preferito
+let firstName = prompt('Scrivi il tuo nome');
+console.log(firstName);
+
+// poi chiedi il suo cognome, (uso prompt per chiedere il cognome all'utente)
+let lastName = prompt('Scrivi il tuo cognome');
+console.log(lastName);
+
+// poi chiedi il suo colore preferito, (uso prompt per chiedere il colore preferito dell'utente)
+let favoriteColor = prompt('Colore preferito?');
+console.log(favoriteColor);
+
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
