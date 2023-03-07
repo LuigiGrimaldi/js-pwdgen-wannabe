836fd4b8840bb49111435d7cbfbb2533516140ef
LuigiGrimaldi <grimaldi1110@gmail.com>
2023-03-07 17:29:28 +0100

    inner commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
 
 
 // Chiedi all'utente il suo nome, (uso prompt per chiedere il nome all'utente)
-let firstName = prompt('Scrivi il tuo nome');
+const firstName = prompt('Scrivi il tuo nome');
 console.log(firstName);
 
 // poi chiedi il suo cognome, (uso prompt per chiedere il cognome all'utente)
-let lastName = prompt('Scrivi il tuo cognome');
+const lastName = prompt('Scrivi il tuo cognome');
 console.log(lastName);
 
 // poi chiedi il suo colore preferito, (uso prompt per chiedere il colore preferito dell'utente)
-let favoriteColor = prompt('Colore preferito?');
+const favoriteColor = prompt('Colore preferito?');
 console.log(favoriteColor);
 
 // Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+const password = firstName + lastName + favoriteColor;
+console.log(password);
+document.getElementById('pass');
+let result = document.getElementById('pass');
+console.log(result);
+result.innerHTML = password + '23';
